aaf6eaf8d7f4979fe0aad17fa79193e6f78cdf4d
home <315516440@qq.com>
2017-05-06 15:11:25 +0800

    modify format

--- example.cpp
+++ example.cpp
@@ -7,23 +7,23 @@ typedef std::function<void(int)> T;
 
 void func(int i)
 {
-	cout << i << endl;
+    cout << i << endl;
 }
 
 void func1(int i, int j)
 {
-	cout << i << "," << j << endl;
+    cout << i << "," << j << endl;
 }
 
 int main()
 {
-	ThreadPool<void> threadpool;
-	threadpool.setMaxTask(10);
-	threadpool.start(5);
-	threadpool.run(func, 1);
-	threadpool.run(func1, 2, 3);
-	threadpool.run(func, 3);
-	threadpool.run(func1, 4, 5);
-	threadpool.run(func, 5);
-	return 0;
+    ThreadPool<void> threadpool;
+    threadpool.setMaxTask(10);
+    threadpool.start(5);
+    threadpool.run(func, 1);
+    threadpool.run(func1, 2, 3);
+    threadpool.run(func, 3);
+    threadpool.run(func1, 4, 5);
+    threadpool.run(func, 5);
+    return 0;
 }
--- threadPool.h
+++ threadPool.h
@@ -10,118 +10,118 @@
 template <typename Ret>
 class ThreadPool
 {
-	typedef std::function<Ret()> Func;
+    typedef std::function<Ret()> Func;
 public:
-	ThreadPool() :running_(false), maxTask_(0), mutex_()
-	{
-	}
-	~ThreadPool()
-	{
-		if (running_)
-			stop();
-	}
-	void start(int numThreads)
-	{
-		running_ = true;
-		threads_.reserve(numThreads);
-		for (int i = 0; i < numThreads; ++i)
-		{
-			threads_.push_back(std::shared_ptr<std::thread>(new std::thread(std::bind(&ThreadPool<Ret>::workerThread, this))));
-		}
-	}
-	void stop()
-	{
-		{
-			std::unique_lock<std::mutex> lock(mutex_);
-			running_ = false;
-			notEmpty_.notify_all();
-		}
-		for (auto t : threads_)
-		{
-			t->join();
-		}
-	}
+    ThreadPool() :running_(false), maxTask_(0), mutex_()
+    {
+    }
+    ~ThreadPool()
+    {
+        if (running_)
+            stop();
+    }
+    void start(int numThreads)
+    {
+        running_ = true;
+        threads_.reserve(numThreads);
+        for (int i = 0; i < numThreads; ++i)
+        {
+            threads_.push_back(std::shared_ptr<std::thread>(new std::thread(std::bind(&ThreadPool<Ret>::workerThread, this))));
+        }
+    }
+    void stop()
+    {
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            running_ = false;
+            notEmpty_.notify_all();
+        }
+        for (auto t : threads_)
+        {
+            t->join();
+        }
+    }
 
-	template<typename FUNC, typename... Args>
-	void run(FUNC && task, Args && ...args)
-	{
-		if (threads_.empty())
-		{
-			task(args...);
-		}
-		else
-		{
-			std::unique_lock<std::mutex> lock(mutex_);
-			while (isFull())
-			{
-				notFull_.wait(lock);
-			}
-			std::shared_ptr<TaskManager> f(new TaskManager);
-			f->install(task, std::forward<Args>(args)...);
-			tasks_.push_back(f);
-			notEmpty_.notify_one();
-		}
-	}
+    template<typename FUNC, typename... Args>
+    void run(FUNC && task, Args && ...args)
+    {
+        if (threads_.empty())
+        {
+            task(args...);
+        }
+        else
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            while (isFull())
+            {
+                notFull_.wait(lock);
+            }
+            std::shared_ptr<TaskManager> f(new TaskManager);
+            f->install(task, std::forward<Args>(args)...);
+            tasks_.push_back(f);
+            notEmpty_.notify_one();
+        }
+    }
 
-	void setMaxTask(int maxTask)
-	{
-		maxTask_ = maxTask;
-	}
+    void setMaxTask(int maxTask)
+    {
+        maxTask_ = maxTask;
+    }
 private:
-	bool isFull()
-	{
-		return maxTask_ > 0 && tasks_.size() >= maxTask_;
-	}
-	void workerThread()
-	{
-		while (running_)
-		{
-			std::shared_ptr<TaskManager> task;
-			{
-				std::unique_lock<std::mutex> lock(mutex_);
-				while (tasks_.empty() && running_)
-				{
-					notEmpty_.wait(lock);
-				}
+    bool isFull()
+    {
+        return maxTask_ > 0 && tasks_.size() >= maxTask_;
+    }
+    void workerThread()
+    {
+        while (running_)
+        {
+            std::shared_ptr<TaskManager> task;
+            {
+                std::unique_lock<std::mutex> lock(mutex_);
+                while (tasks_.empty() && running_)
+                {
+                    notEmpty_.wait(lock);
+                }
 
-				if (!tasks_.empty())
-				{
-					task = tasks_.front();
-					tasks_.pop_front();
-					if (maxTask_ > 0)
-						notFull_.notify_one();
-				}
-			}
-			if (task != nullptr)
-				task->load();
-		}
-	}
+                if (!tasks_.empty())
+                {
+                    task = tasks_.front();
+                    tasks_.pop_front();
+                    if (maxTask_ > 0)
+                        notFull_.notify_one();
+                }
+            }
+            if (task != nullptr)
+                task->load();
+        }
+    }
 private:
-	struct TaskManager
-	{
-		template <typename FUNC, typename... Args>
-		void install(FUNC && fun, Args&& ...args)
-		{
-			m_func = [&fun, &args...]
-			{
-				return forward<FUNC>(fun)(forward<Args>(args)...);
-			};
-		}
-		void load()
-		{
-			return m_func();
-		}
-	private:
-		Func m_func;
-	};
+    struct TaskManager
+    {
+        template <typename FUNC, typename... Args>
+        void install(FUNC && fun, Args&& ...args)
+        {
+            m_func = [&fun, &args...]
+            {
+                return forward<FUNC>(fun)(forward<Args>(args)...);
+            };
+        }
+        void load()
+        {
+            return m_func();
+        }
+    private:
+        Func m_func;
+    };
 public:
 private:
-	std::vector<std::shared_ptr<std::thread> > threads_;
-	std::deque<std::shared_ptr<TaskManager>> tasks_;
-	bool running_;
-	unsigned int maxTask_;
+    std::vector<std::shared_ptr<std::thread> > threads_;
+    std::deque<std::shared_ptr<TaskManager>> tasks_;
+    bool running_;
+    unsigned int maxTask_;
 
-	std::mutex mutex_;
-	std::condition_variable notFull_;
-	std::condition_variable notEmpty_;
+    std::mutex mutex_;
+    std::condition_variable notFull_;
+    std::condition_variable notEmpty_;
 };
