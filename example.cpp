95eeefb712d1cb00810c51b31583926f38591a6b
home <315516440@qq.com>
2017-05-06 15:02:31 +0800

    commit example

--- example.cpp
+++ example.cpp
@@ -1,22 +1,29 @@
-#include ¡°threadPool.h¡±
+#include <iostream>
+#include "threadPool.h"
 
 using namespace std;
+
+typedef std::function<void(int)> T;
 
 void func(int i)
 {
 	cout << i << endl;
 }
 
+void func1(int i, int j)
+{
+	cout << i << "," << j << endl;
+}
+
 int main()
 {
-	ThreadPool<void, int> threadpool;
+	ThreadPool<void> threadpool;
 	threadpool.setMaxTask(10);
 	threadpool.start(5);
 	threadpool.run(func, 1);
-	threadpool.run(func, 2);
+	threadpool.run(func1, 2, 3);
 	threadpool.run(func, 3);
-	threadpool.run(func, 4);
+	threadpool.run(func1, 4, 5);
 	threadpool.run(func, 5);
-
 	return 0;
 }
